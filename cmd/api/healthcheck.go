017f83aefa8bade3e3035e2dceae6ff026b47570
long.nguyen18 <long.nguyen18@onemount.com>
2024-04-23 23:40:56 +0700

    Add JSON enconding to Response: :

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJson(w, http.StatusOK, data, nil)
+	if err != nil {
+		http.Error(w, "Exception", http.StatusInternalServerError)
+	}
+
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -15,4 +16,22 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJson(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	dataAsJson, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	dataAsJson = append(dataAsJson, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(dataAsJson)
+
+	return nil
 }
